7ccaa1e71a8e80b0104db6b9f8a909b79af28f0e
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-14 14:24:57 +0200

    feat: initialized and tested all the templates

--- generals/urls.py
+++ generals/urls.py
@@ -3,6 +3,6 @@ from django.urls.resolvers import URLPattern
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name = 'home'),
+    path('', views.home, name = 'home'),
     path('about/', views.about, name = 'about')
 ]
